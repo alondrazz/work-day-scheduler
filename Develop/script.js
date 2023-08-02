7612822bcf56a4c07b13f28b08f041915199526c
alondra zurita <alondrazurita2@gmail.com>
2023-08-01 20:00:55 -0500

    first changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,23 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
- 
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  //container-fluid px-5 <button class="btn saveBtn col-2
+  const containerText = document.querySelector('.px-5');
+  const saveButton = document.querySelector('.savebtn');
+
+  if (containerText) {
+    console.log('Found containerText element:', containerText);
+  } else {
+    console.log('Container element not found.');
+  }
+  
   
   
   // TODO: Add code to apply the past, present, or future class to each time
@@ -26,7 +34,13 @@ $(function () {
   
   
   // TODO: Add code to display the current date in the header of the page.
+  $(document).ready(function() {
+  const currentElement = $('#currentDay');
+  currentElement.text('Time today!')
+   
+  
   var today = dayjs();
   $('#currentDay').text(today.format('MMM D, YYYY'));
+  console.log('current day:', currentDay)
   })
 
