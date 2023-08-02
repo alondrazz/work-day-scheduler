1b22142e189788ebc1e9cbdd97c7096fd9790ff0
alondra zurita <alondrazurita2@gmail.com>
2023-08-02 17:26:13 -0500

    almost done

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
   // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
   // the code isn't run until the browser has finished rendering all the elements
   // in the html.
-$(document).ready(function () {
+$(document).ready(function() {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -10,33 +10,48 @@ $(document).ready(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //container-fluid px-5 <button class="btn saveBtn col-2
-
-  const containerText = $('.px-5');
+  const containerText = $('.p-5');
   const saveButton = $('.saveBtn');
   const agendaText = $('#agenda-notice');
   
-  $(".saveBtn").click(function() {
-    agendaText.html("agenda save");
-  });
-  
   $(".saveBtn").click(function () {
+    const timeBlockId = $(this).closest('.time-block').attr('id');
+    const userInput = $(this).siblings("textarea").val();
+    saveUserInput(timeBlockId, userInput);
+    agendaText.html("agenda save");
     console.log($(this))
     console.log($(this).siblings("textarea").val())
   });
+
+  
+  
+  
   if (containerText.length > 0) {
     console.log('Found containerText element:', containerText);
   } else {
     console.log('Container element not found.');
   }
 
-
-
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  
+    const currentHour = dayjs().hour();
+  
+    $('.time-block').each(function() {
+      const timeBlockHour = parseInt($(this).attr('id').split('-')[1]);
+      const textarea = $(this).find('textarea');
+
+      if (timeBlockHour < currentHour) {
+        textarea.addClass('.past').removeClass('present future');
+      } else if (timeBlockHour === currentHour) {
+        textarea.addClass('.present').removeClass('past future');
+      } else {
+        textarea.addClass('.future').removeClass('past present');
+      }
+  });
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -68,6 +83,6 @@ $(document).ready(function () {
   const today = dayjs();
 
   currentElement.text(today.format('MMM D, YYYY'));
-  console.log('current day:', today.format('MMM D, YYYY'))
-})
+  console.log('current day:', today.format('MMM D, YYYY'));
+});
 
